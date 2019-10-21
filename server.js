9b80ee3493392e911d3ae353a10a78b6c93df1ac
Jumaidil Fadillah <jumaidilfadillah@gmail.com>
2019-10-21 21:02:00 +0700

    commit

--- server.js
+++ server.js
@@ -10,21 +10,21 @@ const server = express()
 const port = process.env.SERVER_PORT || 9600
 
 async function start() {
-    try {
-        server.use(express.urlencoded({ extended: true }))
-        server.use(express.json())
-        server.use(cors())
-        server.use(fileUpload())
-        server.use(logger('dev'))
-        server.use(routes)
-        
-        // start
-        server.listen(port, () => {
-            console.log(`Server running on http://localhost:${port}\n`)
-        })
-    } catch (err) {
-        console.error('An error occured during start server:', err)
-    }
+	try {
+		server.use(express.urlencoded({ extended: true }))
+		server.use(express.json())
+		server.use(cors())
+		server.use(fileUpload())
+		server.use(logger('dev'))
+		server.use(routes)
+
+		// started
+		server.listen(port, () => {
+			console.log(`Server running on http://localhost:${port}\n`)
+		})
+	} catch (err) {
+		console.error('An error occured during start server:', err)
+	}
 }
 
 start()

fea07cda553d71a0e0c95111550fa0dcd3804262
Jumaidil Fadillah <jumaidilfadillah@gmail.com>
2019-10-22 23:09:39 +0700

    Create hotel rooms booking and payment

--- app/models/hotel.js
+++ app/models/hotel.js
@@ -1,16 +1,51 @@
 const conn = require('../../config/db')
 
 module.exports = {
-    getHotel: (data) => {
-        console.log("afjamsnahjdbajbd")
-        return new Promise((resolve, reject) => {
-            conn.query("SELECT * FROM hotel where id_city=? and name=? and price>=? and price<=? " ,[data.id_city, data.name, data.minPrice, data.maxPrice], (err, result) => {
-                if (!err) {
-                    resolve(result)
-                } else {
-                    reject(err)
-                }
-            })
-        })
-    }
+	getHotel: query => {
+		return new Promise((resolve, reject) => {
+			conn.query(
+				query,
+				(err, result) => {
+					if (!err) {
+						resolve(result)
+					} else {
+						reject(err)
+					}
+				}
+			)
+		})
+	},
+	getHotelRooms: query => {
+		return new Promise((resolve, reject) => {
+			conn.query(query, (err, result) => {
+				if (!err) {
+					resolve(result)
+				} else {
+					reject(err)
+				}
+			})
+		})
+	},
+	hotelBooking: data => {
+		return new Promise((resolve, reject) => {
+			conn.query('INSERT INTO hotel_booked SET ?', data, (err, result) => {
+				if (!err) {
+					resolve(result)
+				} else {
+					reject(err)
+				}
+			})
+		})
+	},
+	hotelBookingConfirm: (data, id) => {
+		return new Promise((resolve, reject) => {
+			conn.query('UPDATE hotel_booked SET ? WHERE ?', [data, id], (err, result) => {
+				if (!err) {
+					resolve(result)
+				} else {
+					reject(err)
+				}
+			})
+		})
+	}
 }
